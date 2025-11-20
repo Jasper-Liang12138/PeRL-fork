7a1f5d23f15ac1214f421f9877ebb5e03957ff5e
MikaStars39 <meloxdmy@qq.com>
2025-11-20 10:57:48 +0800

    [del] delete numina and add still

--- perl/data/__init__.py
+++ perl/data/__init__.py
@@ -1,15 +1,13 @@
 from .openr1 import load_openr1_dataset
-from .numina import load_numina_dataset
 from .tinyzero import load_tinyzero_dataset
 from .count_down import load_count_down_dataset
+from .still import load_still_dataset
 
 from transformers import AutoTokenizer
 
 def load_dataset(dataset_name_or_path: str, example_numbers: int = None, tokenizer: AutoTokenizer = None):
     dataset_name_lower = dataset_name_or_path.lower()
-    if "numinamath-tir" in dataset_name_lower:
-        return load_numina_dataset(dataset_name_or_path, example_numbers)
-    elif "openr1" in dataset_name_lower:
+    if "openr1" in dataset_name_lower:
         return load_openr1_dataset(dataset_name_or_path, example_numbers)
     elif "tinyzero" in dataset_name_lower:
         return load_tinyzero_dataset(dataset_name_or_path, example_numbers)
@@ -19,5 +17,7 @@ def load_dataset(dataset_name_or_path: str, example_numbers: int = None, tokeniz
             example_numbers, 
             tokenizer
         )
+    elif "still" in dataset_name_lower:
+        return load_still_dataset(dataset_name_or_path, example_numbers)
     else:
-        raise ValueError(f"暂不支持的数据集: {dataset_name_or_path}")
+        raise ValueError(f"Not supported dataset: {dataset_name_or_path}")
